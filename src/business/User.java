2cd6d1edb2d2c4032507cf3077868483a57cc413
IIsSchulstad <sjoeholm.rs@gmail.com>
2018-04-24 15:16:39 +0200

    User framework update

--- src/business/User.java
+++ src/business/User.java
@@ -5,28 +5,86 @@
  */
 package business;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Bruger
  */
 public class User {
-    private Case c;
-    private Medicine medicine;
-    private Appointment appointment;
-    private int accessLevel;
-    private Log log;
+
+    private int IDNumber;
+    private boolean caseAccess;
+    private boolean medicine;
+    private boolean appointment;
+    private boolean log;
     private String username;
-    
-    
-    private void createCase() {
-        
+    private String password;
+    private List<Case> cases;
+    private List<Appointment> appointments;
+
+    public User(String username, String password, int accessLevel) {
+        this.username = username;
+        this.password = password;
+        cases = new ArrayList<>();
+        appointments = new ArrayList<>();
+    }
+
+    public boolean login(String username, String password) {
+        return true;
+    }
+
+    public boolean createCase(String CPR) {
+        return true;
+    }
+
+    public boolean updateCase(int caseNumber) {
+        return true;
+    }
+
+    public boolean createMedicine(String name, String amount, String dose) {
+        return true;
+    }
+
+    public boolean updateMedicine(Medicine medicine, String amount, String dose) {
+        return true;
+    }
+
+    public boolean createOffer(String residence, Date startDate) {
+        return true;
+    }
+
+    public boolean createOffer(String residence, Date startDate, Date endDate) {
+        return true;
+    }
+
+    public boolean createAppointment(Date date, String CPR, String note) {
+        return true;
+    }
+
+    public boolean updateAppointment(Appointment ap, Date date, String note) {
+        return true;
+    }
+
+    public boolean removeAppointment(Appointment ap) {
+        return true;
+    }
+
+    public boolean createDailyNote() {
+        return true;
     }
     
-    private void updateCase() {
-        
+    public boolean createUser(String un, String pw, int accessLevel){
+        return true;
     }
     
-    private void createNote() {
-        
+    public boolean deleteUser(int IDNumber){
+        return true;
+    }
+    
+    public boolean updateUserAccess(int IDnum, boolean ca, boolean med, boolean ap, boolean log, boolean handleUsers){
+        return true;
     }
 }
