b80e720eaa0f3b809caeb84162692626344a6439
RasmusAlbrektsen <32238291+RasmusAlbrektsen@users.noreply.github.com>
2018-05-02 21:57:08 +0200

    Commandline control
    Basically ripped off the first semester framewor to create a way to run the system. Missing the functionality for the commands.

--- src/business/Bootstrap.java
+++ src/business/Bootstrap.java
@@ -11,4 +11,9 @@ package business;
  */
 public class Bootstrap {
     
+    public static void main(String[] args) {
+        System system = new System();
+        
+        system.run();
+    }
 }
--- src/business/System.java
+++ src/business/System.java
@@ -5,14 +5,81 @@
  */
 package business;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Bruger
  */
 public class System {
+    private Parser parser;
+    private boolean loggedIn = false;
+
+    public System() {
+        parser = new Parser();
+    }
+    
+    
+    public void run() 
+    {            
+        
+        printHelp();
+
+        boolean finished = false;
+        while (! finished) {
+            Command command = parser.getCommand();
+            finished = commandSwitch(command);
+        }
+        java.lang.System.out.println("System closing...");
+    }
     
      public boolean login(String username, String password) {
         return true;
     }
+     
+     private boolean commandSwitch(Command command) 
+    {
+        boolean wantToQuit = false;
+        CommandEnum commandWord = command.getCommandWord();
+
+        switch (commandWord) {
+            
+            case UNKNOWN: java.lang.System.out.println("\nThe command isn't valid, here are the commands: \n");
+            printHelp();
+            break;
+            
+            case CREATE: 
+            break;
+            
+            case UPDATE: 
+            break;
+            
+            case LOGIN:  
+                
+            break;
+            
+            case HELP: printHelp();
+            break;
+            
+            case QUIT: wantToQuit = true;
+            break;
+            
+        }
+        return wantToQuit;
+    }
     
+    /**
+     * Prints a message showing the commands available.
+     */
+    private void printHelp() 
+    {
+        java.lang.System.out.println("Here's a list over the available commands:\n");
+        java.lang.System.out.println("login: Log in to acces the system. \n");
+        java.lang.System.out.println("create: Create a new case or appointment. \n");
+        java.lang.System.out.println("update: Update a case or an appointment \n");
+        java.lang.System.out.println("quit: Exits the system.\n");
+        
+        parser.showCommands();
+        java.lang.System.out.println("Type your command: ");
+    }
 }
